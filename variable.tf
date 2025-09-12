bf2b006fa977dea42fcf4ba1629625f03e531465
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-12 19:05:37 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -21,12 +21,27 @@ default="laurent"
 }
 
 
+
+variable "subscription_id"{
+  type=string
+}
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
 variable "firstname" {
 type=string
-default="restaurent"
+default="restaurant"
 }
 
 variable "lastname" {
 type=string
 default="mcit"
 }
+
+
