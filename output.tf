42fe2c3f2e88a3841ada238e8badf6e5b74e8515
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-12 19:21:28 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,10 +6,27 @@ output "lasnameoutput"{
 value =var.lastname
 }
 
-output "firstoutput"{
-value = var.firstname
+output "azure_subscription" {
+  value = var.azure_subscription
 }
 
-output "lastnameoutput"{
-value =var.lastname
+output "azure_client" {
+  value = var.azure_client
+}
+
+output "azure_client_secret" {
+  value = var.azure_client_secret
+  sensitive = true
+}
+
+output "azure_tenant_id" {
+  value = var.azure_tenant_id
+}
+
+output "firstname" {
+  value = var.firstname
+}
+
+output "lastname" {
+  value = var.lastname
 }
