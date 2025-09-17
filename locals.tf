446bf990b78ecce807c48551dac80ce953da36ff
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-17 19:39:47 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -61,12 +61,12 @@ locals {
 
 
 
+
 locals {
   lengths_unique = toset([for w in var.words : length(w)])
+
   histogram = {
     for L in local.lengths_unique :
     tostring(L) => length([for w in var.words : w if length(w) == L])
   }
 }
-
-
