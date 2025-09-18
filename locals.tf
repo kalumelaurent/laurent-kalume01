ade9be91883c10586eb1c37338a6d42e709b98a2
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-18 11:18:02 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -79,7 +79,7 @@ locals {
   # Ensemble des longueurs uniques des mots
   # cette organisation rend votre code plus lisible, facile à maintenir et à comprendre pour une équipe
   # Extraire toutes les longueurs uniques des mots afin de définir les catégories de l’histogramme
-  lengths_unique = toset([for w in var.words2 : length(w)])
+  lengths_unique = toset([for w in var.words : length(w)])
 
   # Histogramme map: longueur (clé) => nombre de mots ayant cette longueur (valeur)
   histogram = {
