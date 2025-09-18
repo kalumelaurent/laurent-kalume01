abde13a30a40fde942d632089925858b9fb7ad38
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-18 11:45:18 -0400

    Update module.tf

--- module.tf
+++ module.tf
@@ -1,5 +1,1 @@
-module "user_creation" {
-  source   = "./modules/user_creation"  # chemin relatif vers le module
-  username = "alice"           # valeur obligatoire
-  # email_domain est optionnelle, prise par défaut si non précisée
-}
+
