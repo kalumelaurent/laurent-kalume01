2cb83491746d16d7cf9af68788ab7546789fb012
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-18 11:09:35 -0400

    Update module.tf

--- module.tf
+++ module.tf
@@ -1,4 +1,5 @@
-module "username2" {
-  source    = "./modules/user"
-  username2 = "kalume"
+module "example_user" {
+  source   = "./modules/user"
+  username = "kalume"       # Valeur obligatoire à passer
+  # email_domain est optionnel (exemple.com par défaut)
 }
