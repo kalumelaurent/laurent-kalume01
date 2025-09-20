aafca84be9116be1a9fc474015d9d8b4bb4cabd5
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-19 23:56:47 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -169,17 +169,10 @@ output "activities_abbreviation" {
 
 
 
-output "nom_plus_long_nourriture" {
-  value = local.nom_plus_long
-}
-
-
-# Afficher l'index de l'élément le plus long
 output "index_plus_long_nouriture" {
   value = local.index_max
 }
 
-# Afficher le nom de l'élément le plus long
-output "nom_plus_long_nouriture" {
+output "nom_plus_long_nourriture" {
   value = local.nom_plus_long
 }
