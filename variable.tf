3ac3030b19a9b6b471d431df6b7935968d185cc8
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-20 01:16:58 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -207,3 +207,10 @@ variable "nouriture" {
 }
 
 
+# Définir une variable contenant une liste d'activités
+#c’est un exercice pour apprendre à transformer des données dans Terraform, de manière simple et pratique
+#créer l’abréviation à partir des premières lettres de chaque élément en majuscule 
+variable "activities" {
+  type    = list(string)  # Liste de chaînes de caractères
+  default = ["eat", "sleep", "code", "travel", "play"]
+}
