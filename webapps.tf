b5eacfac47795e578ca83a29c97369b55ebb055b
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-24 18:51:15 -0400

    Update webapps.tf

--- webapps.tf
+++ webapps.tf
@@ -1,1 +1,13 @@
-
+# Variables paramétrables SKU et OS
+variable "sku_name" {
+  type    = string
+  default = "P1v2"
+}
+variable "windows" {
+  type    = string
+  default = "Windows"
+}
+variable "linux" {
+  type    = string
+  default = "Linux"
+}
