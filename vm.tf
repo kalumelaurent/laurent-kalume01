88576daa4757e36e346bb541945476dd47d7b96d
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-24 23:43:54 -0400

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,46 +1,46 @@
-# Groupe de ressources à Montréal
-resource "azurerm_resource_group" "example" {
+# 1. Groupe de ressources à Montréal (Canada Central)
+resource "azurerm_resource_group" "kalume" {
   name     = "example-resources-montreal"
-  location = "Canada Central" # Région Montréal/Québec
+  location = "Canada Central" # Correction pour la région Montréal/Québec
 }
 
-# Réseau virtuel interne
-resource "azurerm_virtual_network" "example" {
-  name                = "internal-network"
+# 2. Réseau virtuel dédié, adresse privée
+resource "azurerm_virtual_network" "kami" {
+  name                = "kami-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.kalume.location
+  resource_group_name = azurerm_resource_group.kalume.name
 }
 
-# Sous-réseau pour usage interne
-resource "azurerm_subnet" "example" {
+# 3. Sous-réseau interne
+resource "azurerm_subnet" "kami" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.kalume.name
+  virtual_network_name = azurerm_virtual_network.kami.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# Interface réseau associée au subnet interne
-resource "azurerm_network_interface" "example" {
-  name                = "internal-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+# 4. Interface réseau reliée au subnet privé
+resource "azurerm_network_interface" "kami" {
+  name                = "kami-nic"
+  location            = azurerm_resource_group.kalume.location
+  resource_group_name = azurerm_resource_group.kalume.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.kami.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-# Déploiement de la VM Linux sur réseau interne Montréal
-resource "azurerm_linux_virtual_machine" "example" {
-  name                  = "vm-montreal-internal"
-  resource_group_name   = azurerm_resource_group.example.name
-  location              = azurerm_resource_group.example.location
+# 5. VM Linux Debian sur le réseau privé Montréal
+resource "azurerm_linux_virtual_machine" "kami" {
+  name                  = "debian-vm-montreal"
+  resource_group_name   = azurerm_resource_group.kalume.name
+  location              = azurerm_resource_group.kalume.location
   size                  = "Standard_F2"
   admin_username        = "adminuser"
-  network_interface_ids = [azurerm_network_interface.example.id]
+  network_interface_ids = [azurerm_network_interface.kami.id]
 
   admin_ssh_key {
     username   = "adminuser"
@@ -52,10 +52,11 @@ resource "azurerm_linux_virtual_machine" "example" {
     storage_account_type = "Standard_LRS"
   }
 
+  # Image officielle Debian 11 (Bullseye)
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    publisher = "Debian"
+    offer     = "debian-11"
+    sku       = "11"
     version   = "latest"
   }
 }
