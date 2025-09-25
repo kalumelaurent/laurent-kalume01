b5673ca629e58c4b888cfb5fa8659ea049df3984
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-24 20:29:42 -0400

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,46 +1,46 @@
 # Groupe de ressources à Montréal
-resource "azurerm_resource_group" "kalume" {
+resource "azurerm_resource_group" "example" {
   name     = "example-resources-montreal"
   location = "Canada Central" # Région Montréal/Québec
 }
 
 # Réseau virtuel interne
-resource "azurerm_virtual_network" "kalume" {
+resource "azurerm_virtual_network" "example" {
   name                = "internal-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.kalume.location
-  resource_group_name = azurerm_resource_group.kalume.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 # Sous-réseau pour usage interne
-resource "azurerm_subnet" "kalume" {
+resource "azurerm_subnet" "example" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.kalume.name
-  virtual_network_name = azurerm_virtual_network.kalume.name
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 # Interface réseau associée au subnet interne
-resource "azurerm_network_interface" "kalume" {
+resource "azurerm_network_interface" "example" {
   name                = "internal-nic"
-  location            = azurerm_resource_group.kalume.location
-  resource_group_name = azurerm_resource_group.kalume.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.kalume.id
+    subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 # Déploiement de la VM Linux sur réseau interne Montréal
-resource "azurerm_linux_virtual_machine" "kalume" {
+resource "azurerm_linux_virtual_machine" "example" {
   name                  = "vm-montreal-internal"
-  resource_group_name   = azurerm_resource_group.kalume.name
-  location              = azurerm_resource_group.kalume.location
+  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.example.location
   size                  = "Standard_F2"
   admin_username        = "adminuser"
-  network_interface_ids = [azurerm_network_interface.kalume.id]
+  network_interface_ids = [azurerm_network_interface.example.id]
 
   admin_ssh_key {
     username   = "adminuser"
@@ -59,4 +59,3 @@ resource "azurerm_linux_virtual_machine" "kalume" {
     version   = "latest"
   }
 }
-
