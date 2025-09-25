e1a812c4ee2b3863fcac4ba84647fa6ff3588dac
kalumelaurent <kalumelaurent0123@gmail.com>
2025-09-25 00:20:28 -0400

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,8 +1,21 @@
-resource "azurerm_resource_group" "kalume" {
-  name     = "kami-resources"
-  location = "Canada Central"  # Région Montréal corrigée
+##########################################
+# Provider Azure
+##########################################
+provider "azurerm" {
+  features {}
 }
 
+##########################################
+# 1. Resource Group
+##########################################
+resource "azurerm_resource_group" "kalume" {
+  name     = "kami-resources"
+  location = "Canada Central" # Montréal
+}
+
+##########################################
+# 2. Réseau virtuel
+##########################################
 resource "azurerm_virtual_network" "kami" {
   name                = "kami-network"
   address_space       = ["10.0.0.0/16"]
@@ -10,6 +23,9 @@ resource "azurerm_virtual_network" "kami" {
   resource_group_name = azurerm_resource_group.kalume.name
 }
 
+##########################################
+# 3. Subnet interne
+##########################################
 resource "azurerm_subnet" "kami" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.kalume.name
@@ -17,6 +33,9 @@ resource "azurerm_subnet" "kami" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+##########################################
+# 4. Network Interface (sans IP publique)
+##########################################
 resource "azurerm_network_interface" "kami" {
   name                = "kami-nic"
   location            = azurerm_resource_group.kalume.location
@@ -29,24 +48,40 @@ resource "azurerm_network_interface" "kami" {
   }
 }
 
-# Correctement passer la clé publique SSH en tant que variable string,
-# cela évite l'erreur liée au chemin
-variable "admin_ssh_public_key" {
-  type = string
-  description = "Clé publique SSH admin (pas comme fichier)"
+##########################################
+# 5. Clé SSH (corrigée)
+##########################################
+# Variable pour stocker le chemin de ta clé publique
+# ⚠️ Mets ton chemin absolu si "~" ne fonctionne pas
+variable "ssh_public_key_path" {
+  description = "Chemin vers la clé publique SSH"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
 }
 
-resource "azurerm_linux_virtual_machine" "kami" {
-  name                  = "debian-vm-montreal"
-  resource_group_name   = azurerm_resource_group.kalume.name
-  location              = azurerm_resource_group.kalume.location
-  size                  = "Standard_F2"
-  admin_username        = "adminuser"
-  network_interface_ids = [azurerm_network_interface.kami.id]
+# Lecture de la clé publique
+# pathexpand() traduit "~" en chemin complet
+data "local_file" "ssh_key" {
+  filename = pathexpand(var.ssh_public_key_path)
+}
 
+##########################################
+# 6. Virtual Machine Debian
+##########################################
+resource "azurerm_linux_virtual_machine" "kami" {
+  name                = "debian-vm"
+  resource_group_name = azurerm_resource_group.kalume.name
+  location            = azurerm_resource_group.kalume.location
+  size                = "Standard_B1s" # VM économique pour test
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.kami.id,
+  ]
+
+  # Clé SSH corrigée
   admin_ssh_key {
     username   = "adminuser"
-    public_key = var.admin_ssh_public_key
+    public_key = data.local_file.ssh_key.content
   }
 
   os_disk {
@@ -54,10 +89,19 @@ resource "azurerm_linux_virtual_machine" "kami" {
     storage_account_type = "Standard_LRS"
   }
 
+  # Image officielle Debian
   source_image_reference {
     publisher = "Debian"
     offer     = "debian-11"
     sku       = "11"
     version   = "latest"
   }
+}
+
+##########################################
+# 7. Output : IP privée
+##########################################
+output "private_ip_address" {
+  description = "Adresse IP privée de la VM Debian"
+  value       = azurerm_network_interface.kami.ip_configuration[0].private_ip_address
 }
