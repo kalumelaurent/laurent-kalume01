93a067eae8ff6e9afb07373bb571cf260bc0d604
kalumelaurent <kalumelaurent0123@gmail.com>
2025-10-01 19:10:04 -0400

    Update yaml.tf

--- yaml.tf
+++ yaml.tf
@@ -1,27 +1,28 @@
 
-
 linux_app:
-  - name: app1-rg
-    resource_group: app1-rg
+  - name: magne1-rg
+    resource_group: magne1-rg
     location: canadacentral
     os_type: Linux
     sku_name: B1
-    app_service_name: appservice-app1
-  - name: app2-rg
-    resource_group: app2-rg
-    location: canadacentral
-    os_type: Linux
-    sku_name: B1
-    app_service_name: appservice-app2
+    app_service_name: magne1-app
 
 
 linux_app:
-  - name: app3-rg
-    resource_group: app3-rg
+  - name: magne2-rg
+    resource_group: magne2-rg
     location: canadacentral
     os_type: Linux
     sku_name: B1
-    app_service_name: appservice-app3
+    app_service_name: magne2-app
+
+linux_app:
+  - name: magne3-rg
+    resource_group: magne3-rg
+    location: canadacentral
+    os_type: Linux
+    sku_name: B1
+    app_service_name: magne3-app
 
 
 locals {
@@ -29,24 +30,23 @@ locals {
     for f in fileset("${path.module}/configs", "[^_]*.yaml") :
       yamldecode(file("${path.module}/configs/${f}"))
   ]
+
   linux_app_list = flatten([
     for app in local.linux_app : [
       for linuxapps in try(app.linux_app, []) : {
-        name                  = linuxapps.name
-        resource_group_name   = linuxapps.resource_group
-        location              = linuxapps.location
-        os_type               = linuxapps.os_type
-        sku_name              = linuxapps.sku_name
-        app_service_name      = linuxapps.app_service_name
+        name                = linuxapps.name
+        resource_group_name = linuxapps.resource_group
+        location            = linuxapps.location
+        os_type             = linuxapps.os_type
+        sku_name            = linuxapps.sku_name
+        app_service_name    = linuxapps.app_service_name
       }
     ]
   ])
 }
 
 
-
-
-resource "azurerm_resource_group" "this" {
+resource "azurerm_resource_group" "mcitexamplerg" {
   for_each = { for rg in local.linux_app_list : rg.resource_group_name => rg }
   name     = each.value.resource_group_name
   location = each.value.location
@@ -57,7 +57,7 @@ resource "azurerm_app_service_plan" "this" {
   for_each            = { for app in local.linux_app_list : app.app_service_name => app }
   name                = "plan-${each.value.app_service_name}"
   location            = each.value.location
-  resource_group_name = azurerm_resource_group.this[each.value.resource_group_name].name
+  resource_group_name = azurerm_resource_group.mcitexamplerg[each.value.resource_group_name].name
   kind                = "Linux"
   reserved            = true
 
@@ -67,12 +67,10 @@ resource "azurerm_app_service_plan" "this" {
   }
 }
 
-
 resource "azurerm_linux_web_app" "this" {
   for_each            = { for app in local.linux_app_list : app.app_service_name => app }
   name                = each.value.app_service_name
   location            = each.value.location
-  resource_group_name = azurerm_resource_group.this[each.value.resource_group_name].name
+  resource_group_name = azurerm_resource_group.mcitexamplerg[each.value.resource_group_name].name
   service_plan_id     = azurerm_app_service_plan.this[each.value.app_service_name].id
 }
-
