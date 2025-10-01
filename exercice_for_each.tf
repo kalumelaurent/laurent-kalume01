e3352660f1a5f14ca55599eb4f293c06290c55fb
kalumelaurent <kalumelaurent0123@gmail.com>
2025-10-01 19:45:49 -0400

    Update exercice_for_each.tf

--- exercice_for_each.tf
+++ exercice_for_each.tf
@@ -1,1 +1,16 @@
+# Déclarer un local ou variable avec le nom et la région pour chaque RG
+locals {
+  resource_groups = {
+    "rg-dev"  = "eastus"
+    "rg-test" = "eastus"
+    "rg-prod" = "eastus"
+  }
+}
 
+# Création multiple de groupes de ressources avec for_each sur la map
+resource "azurerm_resource_group" "rg" {
+  for_each = local.resource_groups
+
+  name     = each.key         # Les clés : rg-dev, rg-test, rg-prod
+  location = each.value       # La valeur : "eastus" pour tous
+}
