9f5f3dbc553f6c0ef019ca4406349b2a531e9f6a
kalumelaurent <kalumelaurent0123@gmail.com>
2025-10-03 20:21:05 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -25,7 +25,10 @@ provider "azurerm" {
 
 variable "subscription_id" { type = string }
 variable "client_id" { type = string }
-variable "client_secret" { type = string, sensitive = true }
+variable "client_secret" { 
+type = string 
+sensitive = true 
+}
 variable "tenant_id" { type = string }
 
 variable "location" {
