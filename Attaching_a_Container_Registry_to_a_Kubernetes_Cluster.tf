a84ee76dd90801f9e5613445d4dfe44820eb657e
kalumelaurent <kalumelaurent0123@gmail.com>
2025-10-06 19:23:41 -0400

    Update Attaching_a_Container_Registry_to_a_Kubernetes_Cluster.tf

--- Attaching_a_Container_Registry_to_a_Kubernetes_Cluster.tf
+++ Attaching_a_Container_Registry_to_a_Kubernetes_Cluster.tf
@@ -1,1 +1,54 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+
+resource "azurerm_container_registry" "acr" {
+  name                = "exampleregistry1234"   # Le nom doit être globalement unique et ne contenir que des lettres minuscules et chiffres
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Standard"              # Standard, Basic ou Premium
+  admin_enabled       = false                    # Recommandé pour la production
+}
+
+
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned" # Obligatoire pour permettre Azure RBAC ci-dessous
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.example.kubelet_identity[0].object_id
+}
+
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.example.kube_config_raw
+  sensitive = true
+}
 
